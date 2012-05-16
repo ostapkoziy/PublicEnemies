5c329e19c3e160c5605ed86fdab38a3e57c573e7
Danylo_Batyuk <Danylo_Batyuk@192.168.12.75>
2012-05-16 11:56:07 +0300

    casino jibat

---
+++ PublicEnemies/src/main/java/com/epam/publicenemies/web/casino/blackjack/BlackJackGameController.java
@@ -0,0 +1,19 @@
+package com.epam.publicenemies.web.casino.blackjack;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class BlackJackGameController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		String a =request.getParameter("chips");
+		System.out.println(a);
+		return new ModelAndView("blackJackGame");
+	}
+
+}
