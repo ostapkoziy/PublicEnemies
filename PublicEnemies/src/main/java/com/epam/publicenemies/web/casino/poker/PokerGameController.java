7aee71e62c9dd08f4d7c08f700457d131e4e1339
Ostap_Koziy <Ostap_Koziy@192.168.12.186>
2012-05-17 15:11:34 +0300

    updated card images and poker started

--- PublicEnemies/src/main/java/com/epam/publicenemies/web/casino/poker/PokerGameController.java
+++ PublicEnemies/src/main/java/com/epam/publicenemies/web/casino/poker/PokerGameController.java
@@ -1,6 +1,26 @@
 package com.epam.publicenemies.web.casino.poker;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.epam.publicenemies.domain.Aid;
+import com.epam.publicenemies.domain.Armor;
+import com.epam.publicenemies.domain.Profile;
+import com.epam.publicenemies.domain.Weapon;
+import com.epam.publicenemies.service.IProfileManagerService;
+import com.epam.publicenemies.service.IShopManagerService;
+
+
+
+/*import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
@@ -14,4 +34,63 @@ public class PokerGameController extends AbstractController {
 		return new ModelAndView("pokerGame");
 	}
 
+}
+*/
+/**
+ * Manages shop page
+ * 
+ * @author Ostap Koziy
+ */
+@Controller
+@RequestMapping("/pokerGame")
+public class PokerGameController {
+	
+	private Logger log = Logger.getLogger(PokerGameController.class); 
+	
+	@Autowired
+	@Qualifier("profileManagerService")
+	private IProfileManagerService	profileManagerService;
+	
+
+	public void setProfileManagerService(IProfileManagerService profileManagerService) {
+		this.profileManagerService = profileManagerService;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView showShopForm(HttpServletRequest req) {
+		log.info("Poker page is showing (get method)"); 
+		ModelAndView mav = new ModelAndView(); 
+
+		Profile pd = profileManagerService.getProfileByUserId((Integer) req.getSession().getAttribute("userId"));
+		if (pd != null) {
+			log.info("Profile has been fetched successfully");
+		} else {
+			log.info("USER NOT FOUND!");
+		}
+		
+		mav.addObject("profile", pd); 
+
+		
+		mav.setViewName("pokerGame"); 
+		return mav;	
+	}
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView postPoker(HttpServletRequest req) {
+		log.info("Poker page is showing (get method)"); 
+		ModelAndView mav = new ModelAndView(); 
+		String chips = req.getParameter("chips");
+		mav.addObject("chips",chips);
+		Profile pd = profileManagerService.getProfileByUserId((Integer) req.getSession().getAttribute("userId"));
+		if (pd != null) {
+			log.info("Profile has been fetched successfully");
+		} else {
+			log.info("USER NOT FOUND!");
+		}
+		
+		mav.addObject("profile", pd); 
+
+		
+		mav.setViewName("pokerGame"); 
+		return mav;	
+	}
 }
--- PublicEnemies/src/main/webapp/WEB-INF/jsp/pokerGame.jsp
+++ PublicEnemies/src/main/webapp/WEB-INF/jsp/pokerGame.jsp
@@ -1,12 +1,46 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<link rel="stylesheet" href="css/poker.css" type="text/css">
+<script type="text/javascript" src="js/jquery-1.7.2.js"></script>
+<!-- <script language="javascript" src="js/regular.js"></script> -->
+<title>Poker</title>
 </head>
 <body>
-nothing
+	<table align="center">
+		<tr>
+			<td align="center"><img src="img/gamelogo.png"></img></td>
+		</tr>
+		<tr>
+			<table id="poker_table" class="sample" align="center" width="1000" height="667">
+				<tr align="center" height="90">
+					<td>
+						<img style="position:relative; top:90px; left:140px; width:80px; height:80px"  border="2" SRC="img/avatars/default.png">
+					</td>
+					<td>
+						<h4 style="position:relative; top:140px; left:-130px; width:80px; height:80px">1000</h4>
+					</td>
+				</tr>
+				<tr align="center" height="300">
+					<td colspan="2">
+						DICK
+					</td>
+
+				</tr>
+				<tr align="center" height="200">
+					<td>
+						<h4 style="position:relative; top:-130px; left:90px; width:80px; height:80px">${chips}</h4>
+					</td>
+					<td>
+						<img style="position:relative; top:-130px; left:-140px; width:80px; height:80px"  border="2" SRC="${profile.getAvatar()}">
+					</td>
+				</tr>
+			</table>
+		</tr>
+	</table>
 </body>
 </html>
