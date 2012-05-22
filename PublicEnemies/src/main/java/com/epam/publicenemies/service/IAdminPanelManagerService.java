5b3ed4c4b4385c584a8d356c803a816701b63a6b
Chetyrkin_Sviatoslav <Chetyrkin_Sviatoslav@192.168.12.205>
2012-05-22 16:24:29 +0300

    admin panel

--- PublicEnemies/src/main/java/com/epam/publicenemies/service/IAdminPanelManagerService.java
+++ PublicEnemies/src/main/java/com/epam/publicenemies/service/IAdminPanelManagerService.java
@@ -1,8 +1,29 @@
 package com.epam.publicenemies.service;
 
+import java.util.List;
+
+import com.epam.publicenemies.domain.User;
 
 public interface IAdminPanelManagerService {
+	
+	/**
+	 * Get amount of registered users
+	 * @return amount of registered users
+	 */
 	public int getUsersNumber(); 
+	
+	/**
+	 * Get last 5 registered users
+	 * @return List of 5 users
+	 */
+	public List<User> getNewUsers();
+	
+	/**
+	 * Get list of users by their ids
+	 * @param usersid - list of users ids
+	 * @return list of users
+	 */
+	public List<User> getUsers(List<Integer> usersid);
 	
 
 }
