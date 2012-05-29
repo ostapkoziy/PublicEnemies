7adad7555b2dc77ee1e890d8d6ab04434fe9acd1
Ivanov-Alexander <xeron07@mail.ru>
2012-05-30 00:08:59 +0300

    log

---
+++ PublicEnemies/src/main/java/com/epam/publicenemies/utils/Logar.java
@@ -0,0 +1,12 @@
+package com.epam.publicenemies.utils;
+public class Logar
+{
+	public static void main(String[] args)
+	{
+		// double step = Math.log(9) / Math.log(3);
+		// System.out.println(step);
+		int exp = 32000;
+		double lvl = Math.log(exp / 1000) / Math.log(2);
+		System.out.println(lvl);
+	}
+}
