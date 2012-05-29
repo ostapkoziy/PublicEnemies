172f98334b519e11a3345adc91a0efbea12c397f
Chetyrkin_Sviatoslav <Chetyrkin_Sviatoslav@192.168.12.205>
2012-05-29 15:19:28 +0300

    "value" changes in Rating

--- PublicEnemies/src/main/java/com/epam/publicenemies/dao/impl/UserDaoImpl.java
+++ PublicEnemies/src/main/java/com/epam/publicenemies/dao/impl/UserDaoImpl.java
@@ -462,7 +462,7 @@ public class UserDaoImpl implements IUserDao {
 	 */
 	@Override
 	public List<Map<String, Object>> getUsersSortedByExperience() {
-		final String SELECT_SQL = "SELECT nickName, experience FROM users, characters WHERE userCharacter=characterId ORDER BY experience";
+		final String SELECT_SQL = "SELECT nickName, experience AS value FROM users, characters WHERE userCharacter=characterId ORDER BY experience";
 		List<Map<String, Object>> list = jdbcTemplate.queryForList(SELECT_SQL);
 		log.info("UserDaoImpl.getUsersSortedByExperience : "+list.size()+" users were fetched");
 		return list;
@@ -474,7 +474,7 @@ public class UserDaoImpl implements IUserDao {
 	 */
 	@Override
 	public List<Map<String, Object>> getUsersSortedByMoney() {
-		final String SELECT_SQL = "SELECT nickName, money FROM users ORDER BY money";
+		final String SELECT_SQL = "SELECT nickName, money AS value FROM users ORDER BY money";
 		List <Map<String, Object>> list = jdbcTemplate.queryForList(SELECT_SQL);
 		log.info("UserDaoImpl.getUsersSortedByMoney : "+list.size()+" users were fetched");
 		return list;
