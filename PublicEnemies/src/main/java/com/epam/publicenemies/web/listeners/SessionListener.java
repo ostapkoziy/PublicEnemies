2d78a71a6daefb5ea091952acf2d286bd4bf4a4b
Danylo_Batyuk <Danylo_Batyuk@192.168.12.75>
2012-05-30 13:14:28 +0300

    blackjack

--- PublicEnemies/src/main/java/com/epam/publicenemies/web/listeners/SessionListener.java
+++ PublicEnemies/src/main/java/com/epam/publicenemies/web/listeners/SessionListener.java
@@ -4,8 +4,14 @@ import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+import com.epam.publicenemies.domain.Profile;
+import com.epam.publicenemies.domain.blackjack.BlackJackGame;
+import com.epam.publicenemies.domain.blackjack.BlackJackGameList;
 import com.epam.publicenemies.domain.fight.Fight;
+import com.epam.publicenemies.service.IProfileManagerService;
 import com.epam.publicenemies.utils.Utils;
 
 /**
@@ -15,6 +21,21 @@ import com.epam.publicenemies.utils.Utils;
  */
 public class SessionListener implements HttpSessionListener
 {
+	@Autowired
+	private IProfileManagerService profileManagerService;
+
+	public void setProfileManagerService(
+			IProfileManagerService profileManagerService) {
+		this.profileManagerService = profileManagerService;
+	}
+
+	@Autowired
+	@Qualifier("games")
+	private BlackJackGameList games;
+
+	public void setGames(BlackJackGameList games) {
+		this.games = games;
+	}
 	private static Logger	log	= Logger.getLogger(SessionListener.class);
 	public void sessionCreated(HttpSessionEvent event)
 	{
@@ -26,5 +47,13 @@ public class SessionListener implements HttpSessionListener
 		Fight fight = (Fight) event.getSession().getAttribute("game");
 		String role = (String) event.getSession().getAttribute("gameRole");
 		Utils.isOldGameInSession(fight, role);
+//		BlackJack Game Remove
+		Integer userId = (Integer) event.getSession().getAttribute("userId");
+		Profile profile = profileManagerService.getProfileByUserId(userId);
+		BlackJackGame game = games.getGameById(userId);
+		profileManagerService.updateMoney(userId,
+				profile.getMoney() + game.getChips());
+		log.info("BLACKJACK GAME: " + userId + "  DESTROYED");
+		games.removeGame(userId);
 	}
 }
--- PublicEnemies/src/main/webapp/js/blackjack/casino.js
+++ PublicEnemies/src/main/webapp/js/blackjack/casino.js
@@ -1,7 +1,7 @@
 $(document)
 		.ready(
 				function() {
-					$("#blackjack").click(function(){
+					$("#blackjack").click(function() {
 						var nothing1 = null;
 						var nothing2 = null;
 						var attr = $("#hit_button").attr("src");
@@ -13,76 +13,26 @@ $(document)
 						$.ajax
 						{
 							$.ajax({
-								url : "checkForBlackJackGame.html",
-								data : ({
-									playerNothing1 : nothing1,
-									playerNothing2 : nothing2
-								}),
-								success : function(data) {
-									var game = jQuery.parseJSON(data);
-									if(game != null){
-									window.location.replace("blaBlaBlackJackController.html");
-									allDataUpdate(game);}
-								},
-								error : function(e, ajaxOptions, thrownError) {
-									alert(e.status);
-									alert(thrownError);
-									alert("Error in dealSend()");
-								}
+										url : "checkForBlackJackGame.html",
+										data : ({
+											playerNothing1 : nothing1,
+											playerNothing2 : nothing2
+										}),
+										success : function(data) {
+											var game = jQuery.parseJSON(data);
+											if (game != null) {
+												window.location.replace("/PublicEnemies/emptyBlackJack.html");
+											}
+										},
+										error : function(e, ajaxOptions,
+												thrownError) {
+											alert(e.status);
+											alert(thrownError);
+											alert("Error in dealSend()");
+										}
 
-							});
+									});
 						}
 
 					}
-
-					function allDataUpdate(game) {
-						var i = 0;
-						for (i = 0; i < game.round.playerCards.length; i = i + 1) {
-							$("#player_cards").append(
-									"<img src="
-											+ game.round.playerCards[i].image
-											+ "></img>");
-						}
-						$("#player_cardsSplit").empty();
-						for (i = 0; i < game.round.playerCardsSplit.length; i = i + 1) {
-							$("#player_cardsSplit").append(
-									"<img src="
-											+ game.round.playerCardsSplit[i].image
-											+ "></img>");
-						}
-						$("#dealer_cards").empty();
-						for (i = 0; i < game.round.dealerCards.length; i = i + 1) {
-							$("#dealer_cards").append(
-									"<img src="
-											+ game.round.dealerCards[i].image
-											+ "></img>");
-						}
-						$("#player_points").empty().append(
-								game.round.playerPoints);
-						$("#result").empty().append(game.round.playerResult);
-
-						// Chips
-						$("#playerChips").empty().append(game.chips);
-						$("#playerBet").empty().append(game.round.playerBet);
-
-						// Buttons
-						if (game.round.playerResult == null) {
-							if (game.round.playerCards[0].rank == game.round.playerCards[1].rank)
-								$("#split_button").attr("src",
-										"img/layout/split.png");
-							$("#deal_button").attr("src",
-									"img/layout/dealg.png");
-							$("#stand_button").attr("src",
-									"img/layout/stand.png");
-							$("#hit_button").attr("src", "img/layout/hit.png");
-							$("#double_button").attr("src",
-									"img/layout/double.png");
-						} else {
-							$("#deal_button").attr("src",
-									"img/layout/rebeat.png");
-						}
-					}
-
-
-
 				});
