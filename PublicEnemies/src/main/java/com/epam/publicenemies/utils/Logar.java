402512db56c2c77fdfb5c2146695957006d5f5a6
Ivanov-Alexander <xeron07@mail.ru>
2012-05-30 16:35:32 +0300

    pom.xml update

--- PublicEnemies/src/main/java/com/epam/publicenemies/utils/Logar.java
+++ PublicEnemies/src/main/java/com/epam/publicenemies/utils/Logar.java
@@ -10,5 +10,25 @@ public class Logar
 		// double lvl = Math.log(exp / 1000) / Math.log(2);
 		// System.out.println(lvl);
 		// int expForNextLevel = 1000*lvl*lvl*100
+		int leftGr = 0;
+		int rightGr = 0;
+		int lvl = 0;
+		int allExp = 2600;
+		while (true)
+		{
+			lvl++;
+			rightGr = rightGr + 1000 + lvl * lvl * 100;
+			if (rightGr > allExp)
+			{
+				break;
+			}
+			leftGr = rightGr;
+		}
+		int expOnCurrentLvl = allExp - leftGr;
+		System.out.println("ALL EXP: " + allExp);
+		System.out.println("LEVEL: " + lvl);
+		System.out.println("RIGHT: " + rightGr);
+		System.out.println("LEFT: " + leftGr);
+		System.out.println("EXP ON CURRENT LVL: " + expOnCurrentLvl);
 	}
 }
