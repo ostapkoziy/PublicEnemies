87c4c9c46e7d25e057e18ad2617cc045b41abed8
Ostap_Koziy <Ostap_Koziy@192.168.12.186>
2012-06-05 13:18:14 +0300

    blackjack ui updated

--- PublicEnemies/src/main/java/com/epam/publicenemies/web/casino/blackjack/DealBlackJackController.java
+++ PublicEnemies/src/main/java/com/epam/publicenemies/web/casino/blackjack/DealBlackJackController.java
@@ -80,7 +80,7 @@ public class DealBlackJackController {
 		// Take 2 cards for player
 		List<BlackJackCard> playerCards = new ArrayList<BlackJackCard>();
 		for (int i = 0; i < 2; i++) {
-			playerCards.add(deck.getCard());
+			playerCards.add(deck.getBlackJack());
 		}
 
 		// Calculate player points
